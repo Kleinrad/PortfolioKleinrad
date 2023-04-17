f1ffc1b09def61bc47f6f028c183f1244614ab1d
Kleinrad <50015410+Kleinrad@users.noreply.github.com>
2023-04-17 03:11:02 +0200

    added techitem

---
+++ src/components/TechnologyItem.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="tech_item_wrapper">
+        <div class="tech_logo">
+            <div class="tech_color"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TechnologyItem",
+    data() {
+        return {
+            name: "C++",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+            color: "#A1CEB3",
+            letter_count: 0,
+        };
+    },
+    mounted() {
+        this.letter_count = this.name.length;
+        this.logo = "url(" + this.logo + ")";
+    },
+};
+
+</script>
+
+<style>
+.tech_item_wrapper {
+    position: relative;
+    display: inline-block;
+    width: 4vw;
+    height: 4vw;
+    line-height: 4vw;
+    overflow: hidden;
+}
+
+
+.tech_logo {
+    width: 4vw;
+    height: 4vw;
+    background-image: v-bind(logo);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.tech_item_wrapper:hover .tech_color {
+    animation: shrink 0.5s forwards;
+}
+
+.tech_color {
+    width: 100%;
+    height: 100%;
+    background-color: #A1CEB3;
+    mix-blend-mode: color;
+}
+
+@keyframes shrink {
+    0% {
+        height: 4vw;
+    }
+    100% {
+        height: 0vw;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -73,128 +73,128 @@ export default {
     HeaderSideBar,
     CursorDot,
   },
-  methods: {
-    handleScroll(scrollDelta) {
-      this.checkEvent()
-      if(scrollDelta == undefined) return;
-      this.scrollInput = Math.min(Math.max(this.scrollInput + scrollDelta*this.scrollSpeed, 0), this.height);
-      this.scrollCounter = Math.floor(this.scrollInput/(100*this.scrollSpeed))*(100*this.scrollSpeed);
-      this.wrapper_top = this.scrollInput;
-      this.scrollSpeed = 0.5;
-      return;
-    },
-    checkEvent() {
-      this.scrollEvents.every((event, i) => {
-        let elementBounds = document.getElementById(event.id).getBoundingClientRect();
-        let currScore = Math.floor((elementBounds.height+event.top) / (this.scrollSpeed*100))*(this.scrollSpeed*100)-this.scrollCounter;
-        let triggerPoint = Math.floor((elementBounds.height*(1-event.trigger)) / (this.scrollSpeed*100))*(this.scrollSpeed*100);
-        if(currScore == triggerPoint || event.permenent){
+  // methods: {
+  //   handleScroll(scrollDelta) {
+  //     this.checkEvent()
+  //     if(scrollDelta == undefined) return;
+  //     this.scrollInput = Math.min(Math.max(this.scrollInput + scrollDelta*this.scrollSpeed, 0), this.height);
+  //     this.scrollCounter = Math.floor(this.scrollInput/(100*this.scrollSpeed))*(100*this.scrollSpeed);
+  //     this.wrapper_top = this.scrollInput;
+  //     this.scrollSpeed = 0.5;
+  //     return;
+  //   },
+  //   checkEvent() {
+  //     this.scrollEvents.every((event, i) => {
+  //       let elementBounds = document.getElementById(event.id).getBoundingClientRect();
+  //       let currScore = Math.floor((elementBounds.height+event.top) / (this.scrollSpeed*100))*(this.scrollSpeed*100)-this.scrollCounter;
+  //       let triggerPoint = Math.floor((elementBounds.height*(1-event.trigger)) / (this.scrollSpeed*100))*(this.scrollSpeed*100);
+  //       if(currScore == triggerPoint || event.permenent){
           
-          if((this.scrollDirection == 1 && event.on_down_scroll && !event.permenent)
-            || currScore < triggerPoint){
-            event.on_down_scroll();
-          }
-          if((this.scrollDirection == -1 && event.on_up_scroll && !event.permenent)
-            || currScore > triggerPoint){
-            event.on_up_scroll();
-          }
-        }
-        if(event.continuous && currScore <= triggerPoint && elementBounds.bottom >= 0){
-          if(event.on_scroll){
-            event.on_scroll({bounds: elementBounds, triggerPoint: triggerPoint, currScore: currScore});
-          }
-          if(currScore == 0 && event.on_scroll_end){
-            event.on_scroll_end();
-          }
-        }
-        return true;
-      });
-    },
-    scrollIntercept(e) {
-      let scrollDelta = 0;
-      if (this.touchStart != -1) {
-        this.scrollDirection = this.touchStart - e.touches[0].pageY > 0 ? 1 : -1;
-        scrollDelta = Math.round((this.touchStart - e.touches[0].pageY)*2);
-        this.touchStart = e.touches[0].pageY;
-      }else{
-        this.scrollDirection = e.deltaY > 0 ? 1 : -1;
-        scrollDelta = Math.round(e.deltaY);
-      }
-      this.handleScroll(scrollDelta);
-    },
-    startTouchScroll(e) {
-      this.touchStart = e.touches[0].pageY;
-    },
-    endTouchScroll(e) {
-      this.touchStart = -1;
-      this.scrollDirection = 0;
-    },
-  },
-  mounted() {
-    this.scrollEvents =  [
-        {id: "banner", 
-         trigger: 0.4,
-         permenent: true,
-          on_down_scroll: ()=>{
-            this.currMenuPoint = 0;
-            this.showHeaderBase = false;
-            this.dynamic_cursor = false;
-            document.documentElement.style.setProperty("--accent-color", this.colors.accentColor[1]);
-            document.documentElement.style.setProperty("--secondary-color", this.colors.secondaryColor[1]);
-            document.documentElement.style.setProperty("--background-color", this.colors.backgroundColor[1]);
-            document.documentElement.style.setProperty("--font-color", this.colors.fontColor[1]);
-          }, 
-          on_up_scroll: ()=>{
-            this.item_active = -1;
-            this.dynamic_cursor = true;
-            this.showHeaderBase = true;
-            this.currMenuPoint = -1;
-            this.pLine = 0;
-            document.documentElement.style.setProperty("--accent-color", this.colors.accentColor[0]);
-            document.documentElement.style.setProperty("--secondary-color", this.colors.secondaryColor[0]);
-            document.documentElement.style.setProperty("--background-color", this.colors.backgroundColor[0]);
-            document.documentElement.style.setProperty("--font-color", this.colors.fontColor[0]);
-          }
-        },
-        {id: "projects",
-         trigger: -0.1,
-         continuous: true,
-          on_down_scroll: ()=>{
-            this.pLine = 100;
-            this.item_active = -1;
-            this.show_cursor = false;
-          },
-          on_up_scroll: ()=>{
-            this.show_cursor = true;
-            this.item_active = -1;
-          },
-          on_scroll: (obj)=>{
-            let offCenter = Math.max(((obj.bounds.height-(obj.currScore))/obj.bounds.height),0) * (this.viewHeight/2);
-            this.project_Dot_pos = Math.min(100,Math.max((obj.triggerPoint-(obj.currScore - offCenter))/obj.triggerPoint*100, 0.2));
-            this.scrollSpeed = 0.2;
-          },
-        },
-      ],
+  //         if((this.scrollDirection == 1 && event.on_down_scroll && !event.permenent)
+  //           || currScore < triggerPoint){
+  //           event.on_down_scroll();
+  //         }
+  //         if((this.scrollDirection == -1 && event.on_up_scroll && !event.permenent)
+  //           || currScore > triggerPoint){
+  //           event.on_up_scroll();
+  //         }
+  //       }
+  //       if(event.continuous && currScore <= triggerPoint && elementBounds.bottom >= 0){
+  //         if(event.on_scroll){
+  //           event.on_scroll({bounds: elementBounds, triggerPoint: triggerPoint, currScore: currScore});
+  //         }
+  //         if(currScore == 0 && event.on_scroll_end){
+  //           event.on_scroll_end();
+  //         }
+  //       }
+  //       return true;
+  //     });
+  //   },
+  //   scrollIntercept(e) {
+  //     let scrollDelta = 0;
+  //     if (this.touchStart != -1) {
+  //       this.scrollDirection = this.touchStart - e.touches[0].pageY > 0 ? 1 : -1;
+  //       scrollDelta = Math.round((this.touchStart - e.touches[0].pageY)*2);
+  //       this.touchStart = e.touches[0].pageY;
+  //     }else{
+  //       this.scrollDirection = e.deltaY > 0 ? 1 : -1;
+  //       scrollDelta = Math.round(e.deltaY);
+  //     }
+  //     this.handleScroll(scrollDelta);
+  //   },
+  //   startTouchScroll(e) {
+  //     this.touchStart = e.touches[0].pageY;
+  //   },
+  //   endTouchScroll(e) {
+  //     this.touchStart = -1;
+  //     this.scrollDirection = 0;
+  //   },
+  // },
+  // mounted() {
+  //   this.scrollEvents =  [
+  //       {id: "banner", 
+  //        trigger: 0.4,
+  //        permenent: true,
+  //         on_down_scroll: ()=>{
+  //           this.currMenuPoint = 0;
+  //           this.showHeaderBase = false;
+  //           this.dynamic_cursor = false;
+  //           document.documentElement.style.setProperty("--accent-color", this.colors.accentColor[1]);
+  //           document.documentElement.style.setProperty("--secondary-color", this.colors.secondaryColor[1]);
+  //           document.documentElement.style.setProperty("--background-color", this.colors.backgroundColor[1]);
+  //           document.documentElement.style.setProperty("--font-color", this.colors.fontColor[1]);
+  //         }, 
+  //         on_up_scroll: ()=>{
+  //           this.item_active = -1;
+  //           this.dynamic_cursor = true;
+  //           this.showHeaderBase = true;
+  //           this.currMenuPoint = -1;
+  //           this.pLine = 0;
+  //           document.documentElement.style.setProperty("--accent-color", this.colors.accentColor[0]);
+  //           document.documentElement.style.setProperty("--secondary-color", this.colors.secondaryColor[0]);
+  //           document.documentElement.style.setProperty("--background-color", this.colors.backgroundColor[0]);
+  //           document.documentElement.style.setProperty("--font-color", this.colors.fontColor[0]);
+  //         }
+  //       },
+  //       {id: "projects",
+  //        trigger: -0.1,
+  //        continuous: true,
+  //         on_down_scroll: ()=>{
+  //           this.pLine = 100;
+  //           this.item_active = -1;
+  //           this.show_cursor = false;
+  //         },
+  //         on_up_scroll: ()=>{
+  //           this.show_cursor = true;
+  //           this.item_active = -1;
+  //         },
+  //         on_scroll: (obj)=>{
+  //           let offCenter = Math.max(((obj.bounds.height-(obj.currScore))/obj.bounds.height),0) * (this.viewHeight/2);
+  //           this.project_Dot_pos = Math.min(100,Math.max((obj.triggerPoint-(obj.currScore - offCenter))/obj.triggerPoint*100, 0.2));
+  //           this.scrollSpeed = 0.2;
+  //         },
+  //       },
+  //     ],
 
-    window.addEventListener("scroll", this.scrollIntercept, { passive: false });
-    window.addEventListener("wheel", this.scrollIntercept, { passive: false });
-    //phone scroll
-    window.addEventListener("touchmove", this.scrollIntercept, { passive: false });
-    window.addEventListener("touchstart", this.startTouchScroll, { passive: false });
-    window.addEventListener("touchend", this.endTouchScroll, { passive: false });
+  //   window.addEventListener("scroll", this.scrollIntercept, { passive: false });
+  //   window.addEventListener("wheel", this.scrollIntercept, { passive: false });
+  //   //phone scroll
+  //   window.addEventListener("touchmove", this.scrollIntercept, { passive: false });
+  //   window.addEventListener("touchstart", this.startTouchScroll, { passive: false });
+  //   window.addEventListener("touchend", this.endTouchScroll, { passive: false });
 
-    this.height = document.getElementsByClassName("scroll_wrapper")[0].scrollHeight;
-    this.viewHeight = window.innerHeight;
+  //   this.height = document.getElementsByClassName("scroll_wrapper")[0].scrollHeight;
+  //   this.viewHeight = window.innerHeight;
 
-    for(let i=0; i<this.scrollEvents.length; i++){
-      this.scrollEvents[i]['top'] = document.getElementById(this.scrollEvents[i].id).getBoundingClientRect().top;
-    }
+  //   for(let i=0; i<this.scrollEvents.length; i++){
+  //     this.scrollEvents[i]['top'] = document.getElementById(this.scrollEvents[i].id).getBoundingClientRect().top;
+  //   }
 
-    this.handleScroll();
-    setInterval(() => {
-      this.wrapperY += (this.wrapper_top - this.wrapperY) * 0.1;
-    }, 1000 / 60);
-  },
+  //   this.handleScroll();
+  //   setInterval(() => {
+  //     this.wrapperY += (this.wrapper_top - this.wrapperY) * 0.1;
+  //   }, 1000 / 60);
+  // },
 };
 </script>
 
